64b6547711f87b2c6e75038f4005f659242e77a4
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-10-23 01:40:56 +0100

    add fiatshamir protocol domain separators

---
+++ agg_kzg/fiatshamir.go
@@ -0,0 +1,9 @@
+package agg_kzg
+
+// Domain Separator to identify the aggregation protocol
+// which calls KZG as a sub protocol
+const DOM_SEP_AGG_PROTOCOL = ""
+
+// Domain Separator to identify the underlying KZG PCS
+// protocol
+const DOM_SEP_EVAL_PROTOCOL = ""
