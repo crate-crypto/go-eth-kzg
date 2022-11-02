f93edb1efa38062acb0d8a5241b22607e0eb55b6
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-11-02 22:18:11 +0000

    use protocol separator only

--- agg_kzg/fiatshamir.go
+++ agg_kzg/fiatshamir.go
@@ -1,9 +1,4 @@
 package agg_kzg
 
-// Domain Separator to identify the aggregation protocol
-// which calls KZG as a sub protocol
-const DOM_SEP_AGG_PROTOCOL = ""
-
-// Domain Separator to identify the underlying KZG PCS
-// protocol
-const DOM_SEP_EVAL_PROTOCOL = ""
+// Domain Separator to identify the protocol
+const DOM_SEP_PROTOCOL = "FSBLOBVERIFY_V1_"
