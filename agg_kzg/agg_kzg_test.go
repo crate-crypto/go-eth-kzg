9fc395bbbe3389c11efbb038290b204903aecf8a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2022-11-14 19:36:12 +0000

    add benchmark to quickly get times to varying blob sizes

--- agg_kzg/agg_kzg_test.go
+++ agg_kzg/agg_kzg_test.go
@@ -27,3 +27,56 @@ func TestProofVerifySmoke(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 }
+
+var globalErr error
+
+func BenchAggProofVerify(num_polynomials int, b *testing.B) {
+	domain := kzg.NewDomain(4096)
+	srs, _ := kzg.NewSRSInsecure(*domain, big.NewInt(1234))
+
+	polys := []kzg.Polynomial{}
+
+	for i := 0; i < num_polynomials; i++ {
+		polys = append(polys, randomPoly(4096))
+	}
+
+	proof, err := BatchOpenSinglePoint(domain, polys, &srs.CommitKey)
+	if err != nil {
+		panic("")
+	}
+
+	b.ResetTimer()
+
+	for n := 0; n < b.N; n++ {
+		err = VerifyBatchOpen(domain, polys, proof, &srs.OpeningKey)
+	}
+
+	globalErr = err
+}
+
+func BenchmarkAggVerify1(b *testing.B)  { BenchAggProofVerify(1, b) }
+func BenchmarkAggVerify2(b *testing.B)  { BenchAggProofVerify(2, b) }
+func BenchmarkAggVerify3(b *testing.B)  { BenchAggProofVerify(3, b) }
+func BenchmarkAggVerify4(b *testing.B)  { BenchAggProofVerify(4, b) }
+func BenchmarkAggVerify5(b *testing.B)  { BenchAggProofVerify(5, b) }
+func BenchmarkAggVerify6(b *testing.B)  { BenchAggProofVerify(6, b) }
+func BenchmarkAggVerify7(b *testing.B)  { BenchAggProofVerify(7, b) }
+func BenchmarkAggVerify8(b *testing.B)  { BenchAggProofVerify(8, b) }
+func BenchmarkAggVerify9(b *testing.B)  { BenchAggProofVerify(9, b) }
+func BenchmarkAggVerify10(b *testing.B) { BenchAggProofVerify(10, b) }
+func BenchmarkAggVerify11(b *testing.B) { BenchAggProofVerify(11, b) }
+func BenchmarkAggVerify12(b *testing.B) { BenchAggProofVerify(12, b) }
+func BenchmarkAggVerify13(b *testing.B) { BenchAggProofVerify(13, b) }
+func BenchmarkAggVerify14(b *testing.B) { BenchAggProofVerify(14, b) }
+func BenchmarkAggVerify15(b *testing.B) { BenchAggProofVerify(15, b) }
+func BenchmarkAggVerify16(b *testing.B) { BenchAggProofVerify(16, b) }
+
+func randomPoly(size int) []fr.Element {
+	res := make([]fr.Element, size)
+	for i := 0; i < size; i++ {
+		var k fr.Element
+		k.SetRandom()
+		res[i] = k
+	}
+	return res
+}
--- agg_kzg/fiatshamir.go
+++ agg_kzg/fiatshamir.go
@@ -2,3 +2,13 @@ package agg_kzg
 
 // Domain Separator to identify the protocol
 const DOM_SEP_PROTOCOL = "FSBLOBVERIFY_V1_"
+
+// // Make this a method!
+// func transcriptReadPoly(byts []byte, size_poly uint) ([]fr.Element, error) {
+//  return nil, nil
+// }
+
+// // Get the blob and read in polys, then add them to the transcript
+// func transcriptReadPolys(byts []byte, num_polys uint, size_of_polys uint) ([][]fr.Element, error) {
+//  return nil, nil
+// }
