670f2cfefcaf41f5270bb8dec09631cf51e13ed3
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-01-26 13:01:00 +0000

    add bench code

--- internal/agg_kzg/agg_kzg_test.go
+++ internal/agg_kzg/agg_kzg_test.go
@@ -27,3 +27,44 @@ func TestProofVerifySmoke(t *testing.T) {
 		t.Errorf(err.Error())
 	}
 }
+
+var num_polys = 16
+var num_evaluations_of_poly = 4096
+
+// Setup code
+var domain = kzg.NewDomain(uint64(num_evaluations_of_poly))
+var srs, _ = kzg.NewSRSInsecure(*domain, big.NewInt(1234))
+
+var polys = GeneratePolys(num_polys, num_evaluations_of_poly)
+var proof, _ = BatchOpenSinglePoint(domain, polys, &srs.CommitKey)
+
+func BenchmarkVerifyAggregate(b *testing.B) {
+
+	for n := 0; n < b.N; n++ {
+		err := VerifyBatchOpen(domain, polys, proof, &srs.OpeningKey)
+		if err != nil {
+			panic("")
+		}
+	}
+}
+
+func GeneratePolys(numPolys int, degree int) [][]fr.Element {
+	polys := make([]kzg.Polynomial, numPolys)
+	for i := 0; i < numPolys; i++ {
+		polys[i] = randPoly(degree)
+	}
+	return polys
+}
+
+func randPoly(polyDegree int) []fr.Element {
+	poly := make([]fr.Element, polyDegree)
+	for i := 0; i < polyDegree; i++ {
+		var eval fr.Element
+		_, err := eval.SetRandom()
+		if err != nil {
+			panic("err is not nil")
+		}
+		poly[i] = eval
+	}
+	return poly
+}
