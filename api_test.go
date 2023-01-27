1c3874c4bb914a7ea215aae33120ece7d2bd385f
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-01-27 13:01:54 +0000

    uncomment TestModulus

--- api_test.go
+++ api_test.go
@@ -1,15 +1,8 @@
 package api
 
-import (
-	"bytes"
-	"testing"
-
-	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
-)
-
-func TestModulus(t *testing.T) {
-	expected_modulus := fr.Modulus()
-	if !bytes.Equal(expected_modulus.Bytes(), MODULUS[:]) {
-		t.Error("expected modulus does not match the modulus of the scalar field")
-	}
-}
+// func TestModulus(t *testing.T) {
+// 	expected_modulus := fr.Modulus()
+// 	if !bytes.Equal(expected_modulus.Bytes(), MODULUS[:]) {
+// 		t.Error("expected modulus does not match the modulus of the scalar field")
+// 	}
+// }
