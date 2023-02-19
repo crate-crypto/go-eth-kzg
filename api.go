9ef7d0009b706c3138d9a0e1f8211e9684c592af
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-19 20:11:22 +0000

    Add `ComputeBlobKZGProof`

--- api.go
+++ api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"math/big"
 
+	"github.com/crate-crypto/go-proto-danksharding-crypto/internal/fiatshamir"
 	"github.com/crate-crypto/go-proto-danksharding-crypto/internal/kzg"
 	"github.com/crate-crypto/go-proto-danksharding-crypto/serialisation"
 )
@@ -119,6 +120,49 @@ func (c *Context) VerifyKZGProof(polynomialKZG serialisation.KZGCommitment, kzgP
 	return kzg.Verify(&polyComm, &proof, c.openKey)
 }
 
+func (c *Context) ComputeBlobKZGProof(blob serialisation.Blob) (serialisation.KZGProof, serialisation.G1Point, serialisation.Scalar, error) {
+	// Deserialisation
+	//
+	// 1. Deserialise the `Blob` into a polynomial
+	//
+	poly, err := serialisation.DeserialiseBlob(blob)
+	if err != nil {
+		return serialisation.KZGProof{}, serialisation.G1Point{}, [32]byte{}, err
+	}
+
+	// 2. Commit to polynomial
+	comms, err := kzg.CommitToPolynomials([]kzg.Polynomial{poly}, c.commitKey)
+	if err != nil {
+		return serialisation.KZGProof{}, serialisation.G1Point{}, [32]byte{}, err
+	}
+
+	// 3. Compute Fiat-Shamir challenge
+	serialisedComm := serialisation.SerialiseG1Point(comms[0])
+	challenge := fiatshamir.ComputeChallenge(blob[:], serialisedComm[:])
+
+	//4. Create opening proof
+	openingProof, err := kzg.Open(c.domain, poly, challenge, c.commitKey)
+	if err != nil {
+		return serialisation.KZGProof{}, serialisation.G1Point{}, [32]byte{}, err
+	}
+
+	// Serialisation
+	//
+	// 5. Serialise values
+	//
+	// Polynomial commitment
+	commitment := comms[0]
+
+	serComm := serialisation.SerialiseG1Point(commitment)
+	//
+	// Quotient commitment
+	serProof := serialisation.SerialiseG1Point(openingProof.QuotientComm)
+	//
+	// Claimed value -- Reverse it to use little endian
+	claimedValueBytes := serialisation.SerialiseScalar(openingProof.ClaimedValue)
+
+	return serProof, serComm, claimedValueBytes, nil
+}
 func (c *Context) ComputeKZGProof(blob serialisation.Blob, inputPointBytes serialisation.Scalar) (serialisation.KZGProof, serialisation.G1Point, serialisation.Scalar, error) {
 	// Deserialisation
 	//
