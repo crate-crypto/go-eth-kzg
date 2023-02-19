ecb40503de758e5c5ad686a2123b0b4a060fc536
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-02-19 20:48:20 +0000

    remove length checks as this is done in gnark library now

--- internal/multiexp/multiexp.go
+++ internal/multiexp/multiexp.go
@@ -1,20 +1,12 @@
 package multiexp
 
 import (
-	"errors"
-
 	"github.com/consensys/gnark-crypto/ecc"
 	curve "github.com/consensys/gnark-crypto/ecc/bls12-381"
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
 func MultiExp(scalars []fr.Element, points []curve.G1Affine) (*curve.G1Affine, error) {
-	len_scalars := len(scalars)
-	len_points := len(points)
-	if len_scalars != len_points {
-		return nil, errors.New("number of scalars != number of points")
-	}
-
 	// If there is no work to do, we return the identity point.
 	// This is not an error, though it would be reasonable to make it so
 	// as our use-case should never encounter this case.
