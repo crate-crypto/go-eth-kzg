68649112cb4d86783428b7c7e32879836ed22255
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-04 20:32:54 +0000

    remove now un-needed code

--- internal/kzg/lagrange.go
+++ internal/kzg/lagrange.go
@@ -2,8 +2,6 @@ package kzg
 
 import (
 	"errors"
-	"fmt"
-	"math/big"
 
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 	"github.com/crate-crypto/go-proto-danksharding-crypto/internal/utils"
@@ -61,38 +59,4 @@ func evaluateLagrangePolynomial(domain *Domain, poly Polynomial, eval_point fr.E
 	result.Mul(tmp, &result)
 
 	return &result, indexInDomain, nil
-}
-
-// This function assumes that one has checked that the index
-// is in the domain.
-// If this is not the case, then the function will return the wrong result
-// or panic
-func polyAtIndex(poly Polynomial, indexInDomain fr.Element) *fr.Element {
-	evalPointU64 := frToIndex(indexInDomain)
-	polyValue := poly[evalPointU64]
-	return &polyValue
-}
-
-// This method should only be called after one has checked that
-// index is in the domain.
-// TODO Maybe we should guard this function such that it can only be
-// TODO called by checking if element is in the domain, then returning
-// TODO a element not in domain error if so. Caller can then check for this error
-func frToIndex(indexInDomain fr.Element) uint64 {
-	// A reasonable assumption to make is that the domain size
-	// can fit within a u64.
-	// Given that our polynomial is not in sparse form
-	// then we would run out of memory when we try to allocate
-	// a polynomial with u64::MAX elements
-	if !indexInDomain.IsUint64() {
-		// We panic because, given that the index is in the domain
-		// and the polynomial is represented in sparse form
-		// then we simply cannot index the polynomial as we are doing below.
-		// It would be a map and not a slice
-		indexBigInt := &big.Int{}
-		indexInDomain.BigInt(indexBigInt)
-		panic(fmt.Errorf("Domain size does not fit within a uint64, size is %d", indexBigInt))
-	}
-
-	return indexInDomain.Uint64()
 }
