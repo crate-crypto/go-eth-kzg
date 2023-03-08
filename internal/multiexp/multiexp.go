0a857d7a1716145745117373c460958b322e0384
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-08 21:02:05 +0000

    add initial comment for MultiExp

--- internal/multiexp/multiexp.go
+++ internal/multiexp/multiexp.go
@@ -6,6 +6,8 @@ import (
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
+// Computes a multi exponentiation -- This is inner product between points and scalars
+// spec: g1LibComb
 func MultiExp(scalars []fr.Element, points []bls12381.G1Affine) (*bls12381.G1Affine, error) {
 	var result bls12381.G1Affine
 	return result.MultiExp(points, scalars, ecc.MultiExpConfig{})
