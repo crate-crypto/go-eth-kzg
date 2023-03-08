584ce86fee68e5df8a5e315a79b6c22c5ca9610a
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-08 19:04:48 +0000

    add trusted setup file to parse compressed G1 and G2 strings

---
+++ api/trusted_setup.go
@@ -0,0 +1,82 @@
+package api
+
+import (
+	_ "embed"
+	"encoding/hex"
+
+	bls12381 "github.com/consensys/gnark-crypto/ecc/bls12-381"
+)
+
+// Hex string for a compressed G1 point without the `0x` prefix
+type G1CompressedHexStr = string
+
+// Hex string for a compressed G2 point without the `0x` prefix
+type G2CompressedHexStr = string
+
+func parseTrustedSetup(setupG1 []G1CompressedHexStr, setupG2 []G2CompressedHexStr) ([]bls12381.G1Affine, []bls12381.G2Affine, error) {
+
+	g1Points, err := parseG1Points(setupG1)
+	if err != nil {
+		return nil, nil, err
+	}
+	g2Points, err := parseG2Points(setupG2)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return g1Points, g2Points, nil
+}
+
+func parseG1Point(hexString string) (*bls12381.G1Affine, error) {
+	var g1Point bls12381.G1Affine
+	serializedPoint, err := hex.DecodeString(hexString)
+	if err != nil {
+		return nil, err
+	}
+	_, err = g1Point.SetBytes(serializedPoint)
+	if err != nil {
+		return nil, err
+	}
+	return &g1Point, nil
+}
+func parseG2Point(hexString string) (*bls12381.G2Affine, error) {
+	var g2Point bls12381.G2Affine
+	serializedPoint, err := hex.DecodeString(hexString)
+	if err != nil {
+		return nil, err
+	}
+	_, err = g2Point.SetBytes(serializedPoint)
+	if err != nil {
+		return nil, err
+	}
+	return &g2Point, nil
+}
+
+func parseG1Points(hexStrings []string) ([]bls12381.G1Affine, error) {
+	numG1 := len(hexStrings)
+	g1Points := make([]bls12381.G1Affine, numG1)
+
+	for i, hexStr := range hexStrings {
+		g1Point, err := parseG1Point(hexStr)
+		if err != nil {
+			return nil, err
+		}
+		g1Points[i] = *g1Point
+	}
+
+	return g1Points, nil
+}
+func parseG2Points(hexStrings []string) ([]bls12381.G2Affine, error) {
+	numG2 := len(hexStrings)
+	g2Points := make([]bls12381.G2Affine, numG2)
+
+	for i, hexStr := range hexStrings {
+		g2Point, err := parseG2Point(hexStr)
+		if err != nil {
+			return nil, err
+		}
+		g2Points[i] = *g2Point
+	}
+
+	return g2Points, nil
+}
