507e8a07562239615370f72df82d53523f68f741
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-08 23:30:53 +0000

    check that commitment is valid even though it is unused

--- api/prove.go
+++ api/prove.go
@@ -38,13 +38,21 @@ func (c *Context) BlobsToCommitments(blobs []serialization.Blob) (serialization.
 }
 
 // Note: This method does not check that the commitment corresponds
-// to the `blob`. One should check this externally or call `BlobToCommitment`
+// to the `blob`.
+// The method does still check that the commitment is a valid commitment.
+// One should check this externally or call `BlobToCommitment`
 func (c *Context) ComputeBlobKZGProof(blob serialization.Blob, serializedComm serialization.Commitment) (serialization.KZGProof, serialization.Scalar, error) {
 	// Deserialization
 	//
 	// 1. Deserialize the `Blob`  into a polynomial
 	//
 	poly, err := serialization.DeserializeBlob(blob)
+	if err != nil {
+		return serialization.KZGProof{}, [32]byte{}, err
+	}
+	// Deserialize the commitment -- we only do this to check
+	// if it is in the correct subgroup
+	_, err = serialization.DeserializeG1Point(serializedComm)
 	if err != nil {
 		return serialization.KZGProof{}, [32]byte{}, err
 	}
