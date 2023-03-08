1ab747b4fc95a3043cb0dc25aac67db48c5b638c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-08 19:28:15 +0000

    move all errors to errors.go file

---
+++ internal/kzg/errors.go
@@ -0,0 +1,13 @@
+package kzg
+
+import "errors"
+
+var (
+	ErrInvalidNbDigests               = errors.New("number of digests is not the same as the number of polynomials")
+	ErrInvalidPolynomialSize          = errors.New("invalid polynomial size (larger than SRS or == 0)")
+	ErrVerifyOpeningProof             = errors.New("can't verify opening proof")
+	ErrVerifyBatchOpeningSinglePoint  = errors.New("can't verify batch opening proof at single point")
+	ErrPolynomialMismatchedSizeDomain = errors.New("domain size does not equal the number of evaluations in the polynomial")
+	ErrMinSRSSize                     = errors.New("minimum srs size is 2")
+	ErrSRSPow2                        = errors.New("srs size must be a power of 2")
+)
