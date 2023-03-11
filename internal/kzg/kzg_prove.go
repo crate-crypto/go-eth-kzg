5f44d9ec909cf265f54cfdef3c6fb1e9be83f972
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-11 16:58:35 +0000

    use clearer function names and add more comments

--- internal/kzg/kzg_prove.go
+++ internal/kzg/kzg_prove.go
@@ -10,7 +10,20 @@ func Open(domain *Domain, p Polynomial, point fr.Element, ck *CommitKey) (Openin
 	if len(p) == 0 || len(p) > len(ck.G1) {
 		return OpeningProof{}, ErrInvalidPolynomialSize
 	}
+
 	outputPoint, indexInDomain, err := domain.evaluateLagrangePolynomial(p, point)
+	if err != nil {
+		return OpeningProof{}, err
+	}
+
+	// Compute the quotient polynomial
+	quotientPoly, err := computeQuotientPoly(*domain, p, indexInDomain, *outputPoint, point)
+	if err != nil {
+		return OpeningProof{}, err
+	}
+
+	// Commit to Quotient polynomial
+	quotientCommit, err := Commit(quotientPoly, ck)
 	if err != nil {
 		return OpeningProof{}, err
 	}
@@ -20,60 +33,76 @@ func Open(domain *Domain, p Polynomial, point fr.Element, ck *CommitKey) (Openin
 		ClaimedValue: *outputPoint,
 	}
 
-	// compute the quotient polynomial
-	quotientPoly, err := dividePolyByXminusA(*domain, p, indexInDomain, res.ClaimedValue, point)
-	if err != nil {
-		return OpeningProof{}, err
-	}
-
-	// commit to Quotient polynomial
-	quotientCommit, err := Commit(quotientPoly, ck)
-	if err != nil {
-		return OpeningProof{}, err
-	}
 	res.QuotientComm.Set(quotientCommit)
 
 	return res, nil
 }
 
-// dividePolyByXminusA computes (f-f(a))/(x-a)
-// TODO rename: DividePolyByLinearOnDomain or DividePolyByLinearVanishing
-func dividePolyByXminusA(domain Domain, f Polynomial, indexInDomain int, fa, a fr.Element) ([]fr.Element, error) {
+// Computes q(X) = f(X) - f(a) / X - a in lagrange form.
+//
+// We refer to q(X) as the quotient polynomial.
+//
+// The division needs to be handled differently if `a` is an element in the domain
+// as this means that one needs to divide by 0. Hence, you will observe that this function
+// will follow a different code-path depending on this condition.
+//
+// Note: Since compute f(a) necessitates one knowing whether `a` is in the domain,
+// this function accepts an `indexInDomain` value which will tell us the index of the
+// element if it is in the domain. See `evaluateLagrangePolynomial`
+func computeQuotientPoly(domain Domain, f Polynomial, indexInDomain int, fa, a fr.Element) ([]fr.Element, error) {
 	if domain.Cardinality != uint64(len(f)) {
 		return nil, ErrPolynomialMismatchedSizeDomain
 	}
 
 	if indexInDomain != -1 {
-		return dividePolyByXminusAOnDomain(domain, f, uint64(indexInDomain))
+		// Note: the uint64 conversion is both semantically correct and safer
+		// than accepting an `int``, since we know it shouldn't be negative
+		// and it should cause a panic, if not checked; uint64(-1) = 2^64 -1
+		return computeQuotientPolyOnDomain(domain, f, uint64(indexInDomain))
 	}
 
-	return dividePolyByXminusAOutsideDomain(domain, f, fa, a)
+	return computeQuotientPolyOutsideDomain(domain, f, fa, a)
 }
 
-func dividePolyByXminusAOutsideDomain(domain Domain, f Polynomial, fa, a fr.Element) ([]fr.Element, error) {
-	// first we compute f-f(a)
-	numer := make([]fr.Element, len(f))
+// Computes q(X) = f(X) - f(a) / X - a in lagrange form where `a` is not in the domain.
+//
+// This function then performs division of two polynomials in evaluation form in the usual way.
+func computeQuotientPolyOutsideDomain(domain Domain, f Polynomial, fa, a fr.Element) ([]fr.Element, error) {
+
+	// Compute the lagrange form the of the numerator f(X) - f(a)
+	// Since f is already in lagrange form, we can compute f(X) - f(a)
+	// by shifting all elements in f(X) by f(a)
+	numerator := make([]fr.Element, len(f))
 	for i := 0; i < len(f); i++ {
-		numer[i].Sub(&f[i], &fa)
+		numerator[i].Sub(&f[i], &fa)
 	}
 
-	// Now compute 1/(roots - a)
-	denom := make([]fr.Element, len(f))
+	// Compute the lagrange form of the denominator X - a
+	// and invert all of the evaluations, since it is the
+	// denominator, we do a batch inversion.
+	denominator := make([]fr.Element, len(f))
 	for i := 0; i < len(f); i++ {
-		denom[i].Sub(&domain.Roots[i], &a)
+		denominator[i].Sub(&domain.Roots[i], &a)
 	}
-	denom = fr.BatchInvert(denom)
+	// Since `a` is not in the domain, we are sure that there
+	// are no zeroes in this inversion.
+	//
+	// Note: even if there was a zero, the gnark-crypto library would skip
+	// it and not panic.
+	denominator = fr.BatchInvert(denominator)
 
+	// Compute q(X)
 	for i := 0; i < len(f); i++ {
-		denom[i].Mul(&denom[i], &numer[i])
+		denominator[i].Mul(&denominator[i], &numerator[i])
 	}
 
-	return denom, nil
+	return denominator, nil
 }
 
-// Divides by X-w^m when w^m is in the domain.
+// Computes f(X) - f(a) / X - a in lagrange form where `a` is in the domain
+//
 // [compute_quotient_eval_within_domain](https://github.com/ethereum/consensus-specs/blob/3a2304981a3b820a22b518fe4859f4bba0ebc83b/specs/deneb/polynomial-commitments.md#compute_quotient_eval_within_domain)
-func dividePolyByXminusAOnDomain(domain Domain, f Polynomial, index uint64) ([]fr.Element, error) {
+func computeQuotientPolyOnDomain(domain Domain, f Polynomial, index uint64) ([]fr.Element, error) {
 	y := f[index]
 	z := domain.Roots[index]
 	invZ := domain.PreComputedInverses[index]
