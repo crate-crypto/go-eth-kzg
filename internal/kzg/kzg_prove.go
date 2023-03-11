d4b3696e0933c39e4b92ce86d8e5cdaa66df2c27
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-11 17:27:16 +0000

    specify which part of the specs `computeQuotientPoly` links to

--- internal/kzg/kzg_prove.go
+++ internal/kzg/kzg_prove.go
@@ -48,7 +48,11 @@ func Open(domain *Domain, p Polynomial, point fr.Element, ck *CommitKey) (Openin
 //
 // Note: Since compute f(z) necessitates one knowing whether `z` is in the domain,
 // this function accepts an `indexInDomain` value which will tell us the index of the
-// element if it is in the domain. See `evaluateLagrangePolynomial`
+// element if it is in the domain, see `evaluateLagrangePolynomial` for when `indexInDomain`
+// is computed.
+//
+// The matching code for this method is in `compute_kzg_proof_impl` where the quotient polynomial
+// is computed.
 func computeQuotientPoly(domain Domain, f Polynomial, indexInDomain int, fz, z fr.Element) ([]fr.Element, error) {
 	if domain.Cardinality != uint64(len(f)) {
 		return nil, ErrPolynomialMismatchedSizeDomain
@@ -99,7 +103,7 @@ func computeQuotientPolyOutsideDomain(domain Domain, f Polynomial, fz, z fr.Elem
 	return denominator, nil
 }
 
-// Computes f(X) - f(z) / X - z in lagrange form where `z` is in the domain
+// Computes f(X) - f(z) / X - z in lagrange form where `z` is in the domain.
 //
 // [compute_quotient_eval_within_domain](https://github.com/ethereum/consensus-specs/blob/3a2304981a3b820a22b518fe4859f4bba0ebc83b/specs/deneb/polynomial-commitments.md#compute_quotient_eval_within_domain)
 func computeQuotientPolyOnDomain(domain Domain, f Polynomial, index uint64) ([]fr.Element, error) {
