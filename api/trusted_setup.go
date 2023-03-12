5c71f8db393cd11d804d1ae4bc4d6345caf1c579
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-03-12 18:20:43 +0000

    add parallel deserialization methods

--- api/trusted_setup.go
+++ api/trusted_setup.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	_ "embed"
+	"sync"
 
 	"encoding/hex"
 
@@ -24,16 +25,16 @@ var (
 )
 
 func parseTrustedSetup(setupG1 []G1CompressedHexStr, setupLagrangeG1 []G1CompressedHexStr, setupG2 []G2CompressedHexStr) ([]bls12381.G1Affine, []bls12381.G1Affine, []bls12381.G2Affine, error) {
-	setupG1Points, err := parseG1Points(setupG1)
+	setupG1Points, err := parseG1PointsPar(setupG1)
 	if err != nil {
 		return nil, nil, nil, err
 	}
-	setupLagrangeG1Points, err := parseG1Points(setupLagrangeG1)
+	setupLagrangeG1Points, err := parseG1PointsPar(setupLagrangeG1)
 	if err != nil {
 		return nil, nil, nil, err
 	}
 
-	g2Points, err := parseG2Points(setupG2)
+	g2Points, err := parseG2PointsPar(setupG2)
 	if err != nil {
 		return nil, nil, nil, err
 	}
@@ -77,6 +78,46 @@ func parseG1Points(hexStrings []string) ([]bls12381.G1Affine, error) {
 	}
 
 	return g1Points, nil
+}
+func parseG1PointsPar(hexStrings []string) ([]bls12381.G1Affine, error) {
+	numG1 := len(hexStrings)
+	g1Points := make([]bls12381.G1Affine, numG1)
+
+	var wg sync.WaitGroup
+	wg.Add(numG1)
+	for i := 0; i < numG1; i++ {
+		go func(_i int) {
+			g1Point, err := parseG1Point(hexStrings[_i])
+			if err != nil {
+				panic(err)
+			}
+			g1Points[_i] = g1Point
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	return g1Points, nil
+}
+func parseG2PointsPar(hexStrings []string) ([]bls12381.G2Affine, error) {
+	numG2 := len(hexStrings)
+	g2Points := make([]bls12381.G2Affine, numG2)
+
+	var wg sync.WaitGroup
+	wg.Add(numG2)
+	for i := 0; i < numG2; i++ {
+		go func(_i int) {
+			g2Point, err := parseG2Point(hexStrings[_i])
+			if err != nil {
+				panic(err)
+			}
+			g2Points[_i] = g2Point
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+
+	return g2Points, nil
 }
 func parseG2Points(hexStrings []string) ([]bls12381.G2Affine, error) {
 	numG2 := len(hexStrings)
