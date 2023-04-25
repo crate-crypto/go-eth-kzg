1aed7fa2e7e063630defeea582f12367b7704b13
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2023-04-25 10:48:14 +0100

    add check for numGoRoutines on MultiExp

--- internal/multiexp/multiexp.go
+++ internal/multiexp/multiexp.go
@@ -14,7 +14,29 @@ import (
 // numGoRoutines is used to configure the amount of concurrency needed. Setting this
 // value to a negative number or 0 will make it default to the number of CPUs.
 //
+// Returns an error if the numGoRoutines exceeds 1024.
+//
 // [g1_lincomb]: https://github.com/ethereum/consensus-specs/blob/50a3f8e8d902ad9d677ca006302eb9535d56d758/specs/deneb/polynomial-commitments.md#g1_lincomb
 func MultiExp(scalars []fr.Element, points []bls12381.G1Affine, numGoRoutines int) (*bls12381.G1Affine, error) {
+	err := isValidNumGoRoutines(numGoRoutines)
+	if err != nil {
+		return nil, err
+	}
 	return new(bls12381.G1Affine).MultiExp(points, scalars, ecc.MultiExpConfig{NbTasks: numGoRoutines})
+}
+
+// isValidNumGoRoutines will return an error if the number
+// of go routines to be used is not Valid.
+//
+// Valid meaning that is less than 1024.
+//
+// 1024 is chosen here as the underlying gnark-crypto library will
+// return an error for more than 1024.
+// Instead of waiting until the user tries to call an algorithm
+// which requires numGoRoutines, we return the error here instead.
+func isValidNumGoRoutines(value int) error {
+	if value >= 1024 {
+		return ErrTooManyGoRoutines
+	}
+	return nil
 }
