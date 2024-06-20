3dc05c2f54d39e74ac67a4746e51af58b8e09a95
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-20 23:22:35 +0100

    chore: add lint ignores

--- api_eip7594.go
+++ api_eip7594.go
@@ -2,22 +2,27 @@ package goethkzg
 
 import "github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 
+//lint:ignore U1000 still fleshing out the API
 func (ctx *Context) ComputeCellsAndKZGProofs(blob *Blob, numGoRoutines int) ([CellsPerExtBlob]*Cell, [CellsPerExtBlob]KZGProof, error) {
 	return [CellsPerExtBlob]*Cell{}, [CellsPerExtBlob]KZGProof{}, nil
 }
 
+//lint:ignore U1000 still fleshing out the API
 func (ctx *Context) computeCellsAndKZGProofsFromPolyCoeff(polyCoeff []fr.Element, numGoRoutines int) ([CellsPerExtBlob]*Cell, [CellsPerExtBlob]KZGProof, error) {
 	return [CellsPerExtBlob]*Cell{}, [CellsPerExtBlob]KZGProof{}, nil
 }
 
+//lint:ignore U1000 still fleshing out the API
 func (ctx *Context) RecoverCellsAndComputeKZGProofs(cellIDs []uint64, cells []*Cell, _proofs []KZGProof, numGoRoutines int) ([CellsPerExtBlob]*Cell, [CellsPerExtBlob]KZGProof, error) {
 	return [CellsPerExtBlob]*Cell{}, [CellsPerExtBlob]KZGProof{}, nil
 }
 
+//lint:ignore U1000 still fleshing out the API
 func (ctx *Context) VerifyCellKZGProof(commitment KZGCommitment, cellID uint64, cell *Cell, proof KZGProof) error {
 	return nil
 }
 
+//lint:ignore U1000 still fleshing out the API
 func (ctx *Context) VerifyCellKZGProofBatch(rowCommitments []KZGCommitment, rowIndices []uint64, columnIndices []uint64, cells []*Cell, proofs []KZGProof) error {
 	return nil
 }
