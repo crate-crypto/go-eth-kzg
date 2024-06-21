f90a8c44c21f5d64b7b4d7d502f6a5429a23fac7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-21 19:21:36 +0100

    chore: rename MultiExp to MultiExpG1 and add MultiExpG2

--- internal/multiexp/multiexp.go
+++ internal/multiexp/multiexp.go
@@ -6,7 +6,7 @@ import (
 	"github.com/consensys/gnark-crypto/ecc/bls12-381/fr"
 )
 
-// MultiExp computes a multi exponentiation -- That is, an inner product between points and scalars.
+// MultiExpG1 computes a multi exponentiation -- That is, an inner product between points and scalars.
 //
 // More precisely, the result is set to scalars[0]*points[0] + ... + scalars[n-1]*points[n-1], where n is the length of both slices
 // If the slices differ in length, this function returns an error.
@@ -17,12 +17,19 @@ import (
 // Returns an error if the numGoRoutines exceeds 1024.
 //
 // [g1_lincomb]: https://github.com/ethereum/consensus-specs/blob/017a8495f7671f5fff2075a9bfc9238c1a0982f8/specs/deneb/polynomial-commitments.md#g1_lincomb
-func MultiExp(scalars []fr.Element, points []bls12381.G1Affine, numGoRoutines int) (*bls12381.G1Affine, error) {
+func MultiExpG1(scalars []fr.Element, points []bls12381.G1Affine, numGoRoutines int) (*bls12381.G1Affine, error) {
 	err := isValidNumGoRoutines(numGoRoutines)
 	if err != nil {
 		return nil, err
 	}
 	return new(bls12381.G1Affine).MultiExp(points, scalars, ecc.MultiExpConfig{NbTasks: numGoRoutines})
+}
+func MultiExpG2(scalars []fr.Element, points []bls12381.G2Affine, numGoRoutines int) (*bls12381.G2Affine, error) {
+	err := isValidNumGoRoutines(numGoRoutines)
+	if err != nil {
+		return nil, err
+	}
+	return new(bls12381.G2Affine).MultiExp(points, scalars, ecc.MultiExpConfig{NbTasks: numGoRoutines})
 }
 
 // isValidNumGoRoutines will return an error if the number
