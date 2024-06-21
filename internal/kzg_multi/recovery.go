a41d0718d62009fc1fb0629c88367bdaf3307ed4
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-06-21 21:46:52 +0100

    chore: remove mention of the word `Cell`

--- internal/kzg_multi/recovery.go
+++ internal/kzg_multi/recovery.go
@@ -7,44 +7,84 @@ import (
 	"github.com/crate-crypto/go-eth-kzg/internal/kzg"
 )
 
+// BlockErasureIndex is used to indicate the index of the block that is missing
+// from the codeword.
+type BlockErasureIndex = uint64
+
+// DataRecovery implements a unique decoding algorithm.
+//
+// The algorithm is not generic and is specific to the use-case where:
+//   - We have block erasures. ie we do not lose data in random locations, but in contiguous chunks.
+//     The chunks themselves are predetermined.
 type DataRecovery struct {
-	rootsOfUnityReduced *kzg.Domain
-	domainExtended      *kzg.Domain
-	scalarsPerCell      int
-	scalarsPerExtBlob   int
-	cellsPerExtBlob     int
+	// rootsOfUnityBlockErasureIndex is a domain that corresponds to the number of blocks
+	// that we can have in the codeword.
+	rootsOfUnityBlockErasureIndex *kzg.Domain
+	domainExtended                *kzg.Domain
+	// blockErasureSize indicates the size of `blocks of evaluations` that
+	// can be missing. For example, if blockErasureSize is 4, then 4 evaluations
+	// can be missing, or 8 or 16.
+	//
+	// This is contrary to a general unique decoding algorithm where the number of
+	// missing elements can be any number and the evaluations do not need to
+	// be in blocks.
+	blockErasureSize int
+	// numScalarsInCodeword is the number of scalars in the codeword
+	// ie the number of scalars we get when we get encode the data.
+	numScalarsInCodeword int
+	// numScalarsInDataWord is the number of scalars in the message
+	// that we encode.
+	numScalarsInDataWord int
+	// expansionFactor is the factor by which the data word or message is expanded
+	expansionFactor int
+	// totalNumBlocks is the total number of blocks(groups of evaluations) in the codeword
+	totalNumBlocks int
 }
 
-func NewDataRecovery(scalarsPerCell, scalarsPerExtBlob, cellsPerExtBlob int) *DataRecovery {
-	rootsOfUnityReduced := kzg.NewDomain(uint64(cellsPerExtBlob))
-	domainExtended := kzg.NewDomain(uint64(scalarsPerExtBlob))
+func NewDataRecovery(blockErasureSize, numScalarsInDataWord, expansionFactor int) *DataRecovery {
+	// Compute the number of scalars that will be in the codeword
+	numScalarsInCodeword := numScalarsInDataWord * expansionFactor
+
+	// Compute the total number of blocks that we will need to
+	// represent the codeword
+	totalNumBlocks := numScalarsInCodeword / blockErasureSize
+
+	rootsOfUnityBlockErasureIndex := kzg.NewDomain(uint64(totalNumBlocks))
+	domainExtended := kzg.NewDomain(uint64(numScalarsInCodeword))
 
 	return &DataRecovery{
-		rootsOfUnityReduced: rootsOfUnityReduced,
-		domainExtended:      domainExtended,
-		scalarsPerCell:      scalarsPerCell,
-		scalarsPerExtBlob:   scalarsPerExtBlob,
-		cellsPerExtBlob:     cellsPerExtBlob,
+		rootsOfUnityBlockErasureIndex: rootsOfUnityBlockErasureIndex,
+		domainExtended:                domainExtended,
+		blockErasureSize:              blockErasureSize,
+		numScalarsInCodeword:          numScalarsInCodeword,
+		totalNumBlocks:                totalNumBlocks,
+		numScalarsInDataWord:          numScalarsInDataWord,
+		expansionFactor:               expansionFactor,
 	}
 }
 
-// Note: These cell indices should not be in bit reversed order
-func (dr *DataRecovery) constructVanishingPolyOnIndices(missingCellIndices []uint64) []fr.Element {
-	rootsOfUnityReduced := kzg.NewDomain(uint64(dr.cellsPerExtBlob))
-
-	missingCellIndexRoot := make([]fr.Element, len(missingCellIndices))
-	for i, index := range missingCellIndices {
-		missingCellIndexRoot[i] = rootsOfUnityReduced.Roots[index]
+// Note: These blockErasure indices should not be in bit reversed order
+func (dr *DataRecovery) constructVanishingPolyOnIndices(missingBlockErasureIndices []BlockErasureIndex) []fr.Element {
+	// Collect all of the roots that are associated with the missing block erasure indices
+	missingBlockErasureIndexRoots := make([]fr.Element, len(missingBlockErasureIndices))
+	for i, index := range missingBlockErasureIndices {
+		missingBlockErasureIndexRoots[i] = dr.rootsOfUnityBlockErasureIndex.Roots[index]
 	}
 
-	shortZeroPoly := vanishingPolyCoeff(missingCellIndexRoot)
+	shortZeroPoly := vanishingPolyCoeff(missingBlockErasureIndexRoots)
 
-	zeroPolyCoeff := make([]fr.Element, dr.scalarsPerExtBlob)
+	zeroPolyCoeff := make([]fr.Element, dr.numScalarsInCodeword)
 	for i, coeff := range shortZeroPoly {
-		zeroPolyCoeff[i*dr.scalarsPerCell] = coeff
+		zeroPolyCoeff[i*dr.blockErasureSize] = coeff
 	}
 
 	return zeroPolyCoeff
+}
+
+// NumBlocksNeededToReconstruct returns the number of blocks that are needed to reconstruct
+// the original data word.
+func (dr *DataRecovery) NumBlocksNeededToReconstruct() int {
+	return dr.numScalarsInDataWord / dr.blockErasureSize
 }
 
 func (dr *DataRecovery) RecoverPolynomialCoefficients(data []fr.Element, missingIndices []uint64) ([]fr.Element, error) {
@@ -77,6 +117,6 @@ func (dr *DataRecovery) RecoverPolynomialCoefficients(data []fr.Element, missing
 
 	// We have a expansion factor of two, so this polynomial being returned
 	// should have its latter half as zeros
-	polyCoeff = polyCoeff[:len(polyCoeff)/2]
+	polyCoeff = polyCoeff[:dr.numScalarsInDataWord]
 	return polyCoeff, nil
 }
