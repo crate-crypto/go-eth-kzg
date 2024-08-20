6d10c2bdf9e371b5a1af4030dc91cf191746f963
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2024-08-20 12:16:34 +0100

    chore: add back `TestSRSConversion`

--- internal/kzg/srs_test.go
+++ internal/kzg/srs_test.go
@@ -10,6 +10,28 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestSRSConversion(t *testing.T) {
+	n := uint64(4096)
+	domain := domain.NewDomain(n)
+	secret := big.NewInt(100)
+	srsMonomial, err := newMonomialSRSInsecureUint64(n, secret)
+	if err != nil {
+		t.Error(err)
+	}
+	srsLagrange, err := newLagrangeSRSInsecure(*domain, secret)
+	if err != nil {
+		t.Error(err)
+	}
+
+	lagrangeSRS := domain.IfftG1(srsMonomial.CommitKey.G1)
+
+	for i := uint64(0); i < n; i++ {
+		if !lagrangeSRS[i].Equal(&srsLagrange.CommitKey.G1[i]) {
+			t.Fatalf("conversion incorrect")
+		}
+	}
+}
+
 func TestLagrangeSRSSmoke(t *testing.T) {
 	size := uint64(4)
 	domain := domain.NewDomain(size)
